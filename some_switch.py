b0740e776a763dbafaf9490e3c0f9ebb2e47216b
SidMarvin <66871853+SidMarvin@users.noreply.github.com>
2025-08-27 17:57:26 +0300

    Update some_switch.py

--- some_switch.py
+++ some_switch.py
@@ -1,6 +1,11 @@
+#1. Скроллбар для элементов
+#2. Левая панель с информацией
+#3. Редактирование
+#4. Выбор адреса
+
 import tkinter as tk
 from tkinter import ttk
-from tkinter import simpledialog
+from tkinter import Frame, Tk, BOTTOM
 import mysql.connector
 from tkinter import messagebox
 from PIL import ImageTk, Image
@@ -12,10 +17,10 @@ switch = switches[0] #будет заменено
 switch_characteristics = switches_characteristics[0] #будет заменен
 
 connection = mysql.connector.connect(
-host="",
-user="",
-passwd="",
-database="")
+host="192.168.58.240",
+user="admin",
+passwd="MiloRd12",
+database="nnc_db")
 cursor = connection.cursor() #ставим курсор
 
 
@@ -28,7 +33,7 @@ def rundom_switch():
     root = tk.Tk()
     root.title(f"Панель управления коммутатором {characteristics_data[0][0]}")
     root.geometry("1400x450")
-    root.resizable(False, False)
+    root.resizable(False, False)    
 
     ports = 52 #сюда пробрасывать переменную по числу портов
     heading = ['№ ', 'Тип порта', 'Описание', 'Настройки']
@@ -37,6 +42,7 @@ def rundom_switch():
     style.theme_use("classic")
     style.configure("Main.Treeview", background="white", foreground="black", font=("Times New Roman", 12), rowheight=40)
     style.configure('Heading', background="black", foreground="white", font=("Times New Roman", 12))
+    
     tree = ttk.Treeview(root, show='headings', style="Main.Treeview")
     tree["columns"] = ("col0", "col1", "col2", "col3")
 
@@ -53,7 +59,8 @@ def rundom_switch():
             tree.column("col2", width=400)
         elif row == 3:
             tree.column("col3", width=800)
-    
+   
+    #порты
     for i in range(ports): #тут число портов
         count = len(main_data)
         if i < count:
@@ -66,9 +73,11 @@ def rundom_switch():
     vsb_y = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
     tree.configure(yscrollcommand=vsb_y.set)
     vsb_y.place(x=1380, y=1, height=410)
+    
     vsb_x = ttk.Scrollbar(root, orient="horizontal", command=tree.xview)
     tree.configure(xscrollcommand=vsb_x.set)
     vsb_x.place(x=250, y=411, width=1125)
+    
     root.mainloop()
 
     
